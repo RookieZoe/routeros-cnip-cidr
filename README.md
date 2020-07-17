150cc24e0dc43d436edef79e80afb5ccf8b9477c
Rookie_Zoe <i@rookiezoe.com>
2020-07-17 14:10:06 +0800

    update generator

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# routeros-cnip-cidr
+# RouterOS CN IP List
+
 CN ip list script generator for MikroTik RouterOS
+
+## To use
+
+```Ros Shell
+/tool fetch https://raw.githubusercontent.com/RookieZoe/routeros-cnip-cidr/master/cn_ip_cidr.rsc dst-path=cn.rsc;
+/import file-name=cn.rsc;
+```
---
+++ generator.bash
@@ -0,0 +1,23 @@
+#! /bin/bash
+WORK_DIR=$(cd $(dirname $0); pwd);
+
+if [ ! -d "$WORK_DIR/tmp" ];then
+  mkdir $WORK_DIR/tmp
+fi
+
+curl -s https://ispip.clang.cn/all_cn.txt -o $WORK_DIR/tmp/all_cn.txt && \
+curl -s https://ispip.clang.cn/all_cn_ipv6.txt -o $WORK_DIR/tmp/all_cn_ipv6.txt && \
+cat > $WORK_DIR/cn_ip_cidr.rsc << EOF
+/log info "Import cn ipv4 cidr list..."
+/ip firewall address-list remove [/ip firewall address-list find list=cn_ip_cidr]
+/ip firewall address-list
+EOF
+cat $WORK_DIR/tmp/all_cn.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/cn_ip_cidr.rsc && \
+cat >> $WORK_DIR/cn_ip_cidr.rsc << EOF
+:if ([:len [/system package find where name="ipv6" and disabled=no]] > 0) do={
+/log info "Import cn ipv6 cidr list..."
+/ipv6 firewall address-list remove [/ipv6 firewall address-list find list=cn_ip_cidr]
+/ipv6 firewall address-list
+EOF
+cat $WORK_DIR/tmp/all_cn_ipv6.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/cn_ip_cidr.rsc && \
+echo "}" >> $WORK_DIR/cn_ip_cidr.rsc
