a4844606d410ba9bae06cca5fb5ef093dda1c46f
Rookie_Zoe <i@rookiezoe.com>
2020-07-17 14:24:56 +0800

    add .gitignore file

--- generator.bash
+++ generator.bash
@@ -7,17 +7,17 @@ fi
 
 curl -s https://ispip.clang.cn/all_cn.txt -o $WORK_DIR/tmp/all_cn.txt && \
 curl -s https://ispip.clang.cn/all_cn_ipv6.txt -o $WORK_DIR/tmp/all_cn_ipv6.txt && \
-cat > $WORK_DIR/cn_ip_cidr.rsc << EOF
+cat > $WORK_DIR/dist/cn_ip_cidr.rsc << EOF
 /log info "Import cn ipv4 cidr list..."
 /ip firewall address-list remove [/ip firewall address-list find list=cn_ip_cidr]
 /ip firewall address-list
 EOF
-cat $WORK_DIR/tmp/all_cn.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/cn_ip_cidr.rsc && \
-cat >> $WORK_DIR/cn_ip_cidr.rsc << EOF
+cat $WORK_DIR/tmp/all_cn.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/dist/cn_ip_cidr.rsc && \
+cat >> $WORK_DIR/dist/cn_ip_cidr.rsc << EOF
 :if ([:len [/system package find where name="ipv6" and disabled=no]] > 0) do={
 /log info "Import cn ipv6 cidr list..."
 /ipv6 firewall address-list remove [/ipv6 firewall address-list find list=cn_ip_cidr]
 /ipv6 firewall address-list
 EOF
-cat $WORK_DIR/tmp/all_cn_ipv6.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/cn_ip_cidr.rsc && \
-echo "}" >> $WORK_DIR/cn_ip_cidr.rsc
+cat $WORK_DIR/tmp/all_cn_ipv6.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/dist/cn_ip_cidr.rsc && \
+echo "}" >> $WORK_DIR/dist/cn_ip_cidr.rsc
